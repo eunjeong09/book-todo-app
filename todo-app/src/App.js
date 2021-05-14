4658dfa4ac30bdce4f41438ce3c0e576ef9c8ccd
eunjeong09 <ejko0911@gmail.com>
2021-05-14 23:10:54 +0900

    Add Insert
    Insert기능 추가, state사용으로 App에서 추가 목록 출력

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
@@ -22,9 +22,25 @@ const App = () => {
     },
   ])
 
+  // 고윳값으로 사용될 id
+  // ref를 사용해서 변수에 담기 -> 렌더링 되는 정보가 아닌 참조되는 값이기 때문에 리렌더링X
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id:nextId.current,
+        text,
+        checked:false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; //nextId 1씩 증가
+    },
+    [todos],
+  )
   return (
   <TodoTemplate>
-    <TodoInsert />
+    <TodoInsert onInsert={onInsert}/>
     <TodoList todos={todos} />
   </TodoTemplate>);
 };
--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    /* 
+    * useCallbackHook
+    * 컴포넌트가 리렌더링 될 때마다 함수를 새로 만드는 것이 아니라, 
+    * 한번 함수를 만들고 재사용
+    */
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue(''); //value 초기화
+
+            //submit 이벤트는 브라우저에서 새로고침 발생시킴 -> 방지하기 위한 함수 작성
+            e.preventDefault();
+        },
+        [onInsert, value],
+    )
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요" />
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder="할 일을 입력하세요"
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
                 <MdAdd />
             </button>

7197de23d777a1f633cb22f9b5b8317ac9962746
eunjeong09 <ejko0911@gmail.com>
2021-05-16 16:31:24 +0900

    Add onRemove Event
    붉은 버튼 클릭시 해당 아이디의 todoListItem 삭제

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useCallback} from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
@@ -6,19 +6,19 @@ import TodoList from "./components/TodoList";
 const App = () => {
   const [todos, setTodos] = useState([
     {
-      id:1,
-      text:'리액트의 기초 알아보기',
+      id: 1,
+      text: '리액트의 기초 알아보기',
       checked: true,
     },
     {
-      id:2,
-      text:'컴포넌트 스타일링 해보기',
-      checked:true
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true
     },
     {
-      id:3,
-      text:'일정관리 앱 만들어보기',
-      checked:false,
+      id: 3,
+      text: '일정관리 앱 만들어보기',
+      checked: false,
     },
   ])
 
@@ -29,20 +29,27 @@ const App = () => {
   const onInsert = useCallback(
     text => {
       const todo = {
-        id:nextId.current,
+        id: nextId.current,
         text,
-        checked:false,
+        checked: false,
       };
       setTodos(todos.concat(todo));
       nextId.current += 1; //nextId 1씩 증가
     },
     [todos],
   )
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },
+    [todos],
+  )
   return (
-  <TodoTemplate>
-    <TodoInsert onInsert={onInsert}/>
-    <TodoList todos={todos} />
-  </TodoTemplate>);
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos}  onRemove={onRemove}/>
+    </TodoTemplate>);
 };
 
 export default App;

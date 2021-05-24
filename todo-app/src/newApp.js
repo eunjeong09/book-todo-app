bc91a1c8f2eb4ad499cf139c35cd9806a2bc4a0e
eunjeong09 <ejko0911@gmail.com>
2021-05-24 23:18:33 +0900

    Update TodoList with useState
    TodoList 출력 부분에 useState 사용해서 출력

--- todo-app/src/newApp.js
+++ todo-app/src/newApp.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './newComponents/TodoTemplate';
 import TodoInsert from './newComponents/TodoInsert';
 import TodoList from './newComponents/TodoList';
 
 const App = () => {
+    const [todos, setTodos] = useState([
+        {
+            id:1,
+            text:'할일 1',
+            checked:true
+        },
+        {
+            id:2,
+            text:'할일 2',
+            checked:true,
+        },
+        {
+            id:3,
+            text:'할일 3',
+            checked:false,
+        }
+    ]);
     return (
         <TodoTemplate>
             <TodoInsert />
-            <TodoList />
+            <TodoList todos={todos} />
         </TodoTemplate>
     )
 }
--- todo-app/src/newComponents/TodoListItem.js
+++ todo-app/src/newComponents/TodoListItem.js
@@ -4,14 +4,24 @@ import {
     MdCheckBox,
     MdRemoveCircleOutline,
 } from 'react-icons/md';
+import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = () => {
-    return(
+const TodoListItem = ({ todo }) => {
+    const { text, checked } = todo;
+
+
+    return (
         <div className="TodoListItem">
-            <div className="checkbox">
+
+            {/* checked 값에 따라 다른 아이콘 출력 */}
+            {/* <div className="checkbox">
                 <MdCheckBoxOutlineBlank />
                 <div className="text">할 일</div>
+            </div> */}
+            <div className={cn('checkbox', { checked })}>
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                <div className="text">{text}</div>
             </div>
             <div className="remove">
                 <MdRemoveCircleOutline />

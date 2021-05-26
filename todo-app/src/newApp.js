2aadee2f30ae20834bc8cf8307796f465665537f
eunjeong09 <ejko0911@gmail.com>
2021-05-26 19:45:50 +0900

    Add insert, submit
    TodoInsert컴포넌트 input, form 이벤트 처리

--- todo-app/src/newApp.js
+++ todo-app/src/newApp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import TodoTemplate from './newComponents/TodoTemplate';
 import TodoInsert from './newComponents/TodoInsert';
 import TodoList from './newComponents/TodoList';
@@ -6,24 +6,37 @@ import TodoList from './newComponents/TodoList';
 const App = () => {
     const [todos, setTodos] = useState([
         {
-            id:1,
-            text:'할일 1',
-            checked:true
+            id: 1,
+            text: '할일 1',
+            checked: true
         },
         {
-            id:2,
-            text:'할일 2',
-            checked:true,
+            id: 2,
+            text: '할일 2',
+            checked: true,
         },
         {
-            id:3,
-            text:'할일 3',
-            checked:false,
+            id: 3,
+            text: '할일 3',
+            checked: false,
         }
     ]);
+
+    const nextId = useRef(4);
+    const onInsert = useCallback(
+        text => {
+            const todo = {
+                id: nextId.current,
+                text,
+                checked: false,
+            };
+            setTodos(todos.concat(todo));
+            nextId.current += 1;
+        }, [todos],
+    );
     return (
         <TodoTemplate>
-            <TodoInsert />
+            <TodoInsert onInsert={onInsert}/>
             <TodoList todos={todos} />
         </TodoTemplate>
     )

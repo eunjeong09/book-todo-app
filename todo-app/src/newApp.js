92b8019087354956dd19a3d2047c5713560824d0
eunjeong09 <ejko0911@gmail.com>
2021-05-29 01:29:29 +0900

    Add React.memo, Use setTodos
    성능 최적화를 위한 수정

--- todo-app/src/newApp.js
+++ todo-app/src/newApp.js
@@ -3,13 +3,13 @@ import TodoTemplate from './newComponents/TodoTemplate';
 import TodoInsert from './newComponents/TodoInsert';
 import TodoList from './newComponents/TodoList';
 
-function createBulkTodos(){
+function createBulkTodos() {
     const array = [];
-    for(let i = 1; i<2500; i++){
+    for (let i = 1; i <= 2500; i++) {
         array.push({
-            id:i,
-            text:  `할 일 ${i}`,
-            checked : false,
+            id: i,
+            text: `할 일 ${i}`,
+            checked: false,
         });
     }
     return array;
@@ -26,21 +26,21 @@ const App = () => {
                 text,
                 checked: false,
             };
-            setTodos(todos.concat(todo));
+            setTodos(todos => todos.concat(todo));
             nextId.current += 1;
         }, [todos],
     );
 
     const onRemove = useCallback(
         id => {
-            setTodos(todos.filter(todo => todo.id !== id));
+            setTodos(todos => todos.filter(todo => todo.id !== id));
         },
         [todos],
     )
 
     const onToggle = useCallback(
         id => {
-            setTodos(
+            setTodos(todos => 
                 todos.map(todo =>
                     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
                 ),

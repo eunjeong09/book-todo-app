1481d47d3eed70c7b732845caff7f1372db3d0e1
eunjeong09 <ejko0911@gmail.com>
2021-06-05 01:27:17 +0900

    Add JsDoc
    JsDoc 모듈설치 후 TodoTemplate컴포넌트에서 테스트.

--- todo-app/src/components/TodoTemplate.js
+++ todo-app/src/components/TodoTemplate.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './TodoTemplate.scss';
 
+/**
+ * Todo를 반환하는 메소드
+ * @param {object} children symbol()
+ * @returns {object} TodoTemplate 객체
+ */
 const TodoTemplate = ({ children }) => {
     return (
         <div className="TodoTemplate">
--- todo-app/src/testComponents/TodoTemplate.js
+++ todo-app/src/testComponents/TodoTemplate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TodoTemplate.scss';
 
+
 const TodoTemplate = ({ children }) => {
     return (
         <div className="TodoTemplate">

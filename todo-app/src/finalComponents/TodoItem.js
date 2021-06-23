48db2d48f02ec16f33e87011ae5749a9f2d5cafc
eunjeong09 <ejko0911@gmail.com>
2021-06-24 07:47:53 +0900

    Add onToggle event

--- todo-app/src/finalComponents/TodoItem.js
+++ todo-app/src/finalComponents/TodoItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {
+    MdCheckBoxOutlineBlank,
+    MdCheckBox,
+    MdRemoveCircleOutline,
+} from 'react-icons/md';
 import cn from 'classnames';
 
-const TodoItem = ({todo, onRemove}) => {
+const TodoItem = ({ todo, onRemove, onToggle }) => {
+    const { id, text, checked } = todo;
     return (
         <div>
-            <div >
-                체크박스
+            <div className={cn("checkbox", { checked })} onClick={() => onToggle(todo.id)}>
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
             </div>
             <div>{todo.text}</div>
             <div onClick={() => onRemove(todo.id)} >-</div>
